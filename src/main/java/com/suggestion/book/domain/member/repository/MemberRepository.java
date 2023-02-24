b6fd9314bc916c781d2c975a60afdcbcde9e97bd
scarlet0o0 <ghd7673@naver.com>
2023-02-24 16:09:16 +0900

    [#2] Feat: MemberRepository 생성

---
+++ src/main/java/com/suggestion/book/domain/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.suggestion.book.domain.member.repository;
+
+import com.suggestion.book.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
