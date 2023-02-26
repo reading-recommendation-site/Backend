55c84f7fa0655aca8468b67f46758c42753ee3e3
scarlet0o0 <ghd7673@naver.com>
2023-02-26 15:56:25 +0900

    [#2] Feat: configuration-processor 연결

--- src/main/java/com/suggestion/book/BookApplication.java
+++ src/main/java/com/suggestion/book/BookApplication.java
@@ -2,7 +2,9 @@ package com.suggestion.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan("com.suggestion.book.global.config.properties")
 @SpringBootApplication
 public class BookApplication {
 
