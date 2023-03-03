d7a4e143abff035323ac60ad02be619e6eeeda2a
scarlet0o0 <ghd7673@naver.com>
2023-03-03 22:03:15 +0900

    [#6] Feat: WebClientConfig 생성

---
+++ src/main/java/com/suggestion/book/global/config/WebClientConfig.java
@@ -0,0 +1,24 @@
+package com.suggestion.book.global.config;
+
+import com.suggestion.book.global.config.properties.ApiProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebClientConfig {
+
+    private final ApiProperties apiProperties;
+
+    @Bean
+    public WebClient WebClientApi(WebClient.Builder webClientBuilder) {
+        return webClientBuilder
+                .clone()
+                .baseUrl(apiProperties.getNaver().getUrl())
+                .defaultHeader("X-Naver-Client-Id", apiProperties.getNaver().getNaverClientId())
+                .defaultHeader("X-Naver-Client-Secret", apiProperties.getNaver().getNaverClientSecret())
+                .build();
+    }
+}
