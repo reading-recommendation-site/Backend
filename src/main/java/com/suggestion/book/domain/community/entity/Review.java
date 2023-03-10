582d21ba0e6ed3e83967f3c4a0f2250fb2a5fd06
scarlet0o0 <ghd7673@naver.com>
2023-03-10 20:41:34 +0900

    [#14] Feat: 생성일자와 수정일자 추가

--- src/main/java/com/suggestion/book/domain/community/entity/Review.java
+++ src/main/java/com/suggestion/book/domain/community/entity/Review.java
@@ -1,16 +1,20 @@
 package com.suggestion.book.domain.community.entity;
 
+import com.suggestion.book.domain.community.dto.ContentsRequestDto;
 import com.suggestion.book.domain.member.entity.Member;
+import com.suggestion.book.domain.model.TimestampEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "review")
-public class Review {
+public class Review extends TimestampEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,5 +40,9 @@ public class Review {
         this.isbn = isbn;
         this.contents = contents;
         this.grade = grade;
+    }
+
+    public void updateContents(ContentsRequestDto contentsRequestDto){
+        this.contents = contentsRequestDto.getContents();
     }
 }
