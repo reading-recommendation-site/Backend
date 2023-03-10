406611abf363ee46115a7be257443e3b92f94c63
scarlet0o0 <ghd7673@naver.com>
2023-03-10 20:42:12 +0900

    [#14] Feat: 리뷰 수정 / 삭제 로직 구현

--- src/main/java/com/suggestion/book/domain/community/service/ReviewService.java
+++ src/main/java/com/suggestion/book/domain/community/service/ReviewService.java
@@ -1,17 +1,25 @@
 package com.suggestion.book.domain.community.service;
 
-import com.suggestion.book.domain.book.dto.BookListResponseDto;
+import com.suggestion.book.domain.community.dto.BookISBNResponseDto;
+import com.suggestion.book.domain.community.dto.ContentsRequestDto;
 import com.suggestion.book.domain.community.dto.ReviewRequestDto;
+import com.suggestion.book.domain.community.entity.Review;
 import com.suggestion.book.domain.community.exception.InvalidISBNException;
+import com.suggestion.book.domain.community.exception.MemberIdMismatchException;
+import com.suggestion.book.domain.community.exception.ReviewNotFoundException;
 import com.suggestion.book.domain.community.repository.ReviewRepository;
 import com.suggestion.book.domain.member.entity.Member;
 import com.suggestion.book.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReviewService {
 
@@ -21,24 +29,45 @@ public class ReviewService {
 
     private static final String URI = "/search/book_adv.json";
 
-    public void createReview(ReviewRequestDto reviewRequestDto, String memberName) {
+    @Transactional
+    public void createReview(ReviewRequestDto reviewRequestDto, String memberId) {
         if(!validateIsbn(reviewRequestDto.isbn)){
-            throw new InvalidISBNException("isbn 이 존재하지 않습니다.");
+            throw new InvalidISBNException("isbn 이 존재 하지 않습니다.");
         }
-        Member member = memberRepository.findByMemberId(memberName);
+        Member member = memberRepository.findByMemberId(memberId);
         reviewRepository.save(reviewRequestDto.toEntity(member));
     }
 
+    @Transactional
+    public void updateReview(Long reviewId, ContentsRequestDto contentsRequestDto, String memberId) {
+        Optional<Review> reviewOpt = reviewRepository.findById(reviewId);
+        Review review = reviewOpt.orElseThrow(() -> new ReviewNotFoundException("리뷰가 존재 하지 않습니다."));
+        if(!memberId.equals(review.getMember().getMemberId())){
+            throw new MemberIdMismatchException("멤버가 불일치 합니다.");
+        }
+        review.updateContents(contentsRequestDto);
+    }
+
+    @Transactional
+    public void deleteReview(Long reviewId, String memberId) {
+        Optional<Review> reviewOpt = reviewRepository.findById(reviewId);
+        Review review = reviewOpt.orElseThrow(() -> new ReviewNotFoundException("리뷰가 존재 하지 않습니다."));
+        if(!memberId.equals(review.getMember().getMemberId())){
+            throw new MemberIdMismatchException("멤버가 불일치 합니다.");
+        }
+        reviewRepository.delete(review);
+    }
+
     private boolean validateIsbn(String isbn){
-        Mono<BookListResponseDto> bookListResponseDtoMono = naverWebClientApi
+        Mono<BookISBNResponseDto> bookISBNResponseDtoMono = naverWebClientApi
                 .get()
                 .uri(uriBuilder -> uriBuilder
                         .path(URI)
                         .queryParam("d_isbn", isbn)
                         .build())
                 .retrieve()
-                .bodyToMono(BookListResponseDto.class);
-        BookListResponseDto bookListResponseDto = bookListResponseDtoMono.block();
-        return bookListResponseDto != null && bookListResponseDto.getTotal() == 1;
+                .bodyToMono(BookISBNResponseDto.class);
+        BookISBNResponseDto bookISBNResponseDto = bookISBNResponseDtoMono.block();
+        return bookISBNResponseDto != null && bookISBNResponseDto.getTotal() == 1;
     }
 }
