baafb84acf0c83318e87a97c50cfef609f6db07b
scarlet0o0 <ghd7673@naver.com>
2023-03-10 20:37:18 +0900

    [#14] Feat: EnableJpaAuditing 설정

--- src/main/java/com/suggestion/book/BookApplication.java
+++ src/main/java/com/suggestion/book/BookApplication.java
@@ -3,8 +3,10 @@ package com.suggestion.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan("com.suggestion.book.global.config.properties")
+@EnableJpaAuditing
 @SpringBootApplication
 public class BookApplication {
 
