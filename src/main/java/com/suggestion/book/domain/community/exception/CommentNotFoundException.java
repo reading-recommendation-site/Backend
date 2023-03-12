81d2b2c9b5c28dde8287fcadf07a91a0ef7d0ada
scarlet0o0 <ghd7673@naver.com>
2023-03-12 20:17:24 +0900

    [#16] Feat: 댓글 NotFound 예외 생성

---
+++ src/main/java/com/suggestion/book/domain/community/exception/CommentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.suggestion.book.domain.community.exception;
+
+public class CommentNotFoundException extends RuntimeException{
+    public CommentNotFoundException(String message) {
+        super(message);
+    }
+}
