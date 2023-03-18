6042ff74f36e2de9de2ccb54f65c17e2a09db75c
scarlet0o0 <ghd7673@naver.com>
2023-03-19 04:24:17 +0900

    [#22] Feat: 좋아요 취소와 조회 로직 구현

--- src/main/java/com/suggestion/book/domain/community/service/LikeService.java
+++ src/main/java/com/suggestion/book/domain/community/service/LikeService.java
@@ -2,6 +2,8 @@ package com.suggestion.book.domain.community.service;
 
 import com.suggestion.book.domain.community.entity.Like;
 import com.suggestion.book.domain.community.entity.Review;
+import com.suggestion.book.domain.community.exception.LikeNotFoundException;
+import com.suggestion.book.domain.community.exception.MemberIdMismatchException;
 import com.suggestion.book.domain.community.exception.ReviewNotFoundException;
 import com.suggestion.book.domain.community.repository.LikeRepository;
 import com.suggestion.book.domain.community.repository.ReviewRepository;
@@ -9,24 +11,45 @@ import com.suggestion.book.domain.member.entity.Member;
 import com.suggestion.book.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class LikeService {
     private final LikeRepository likeRepository;
     private final ReviewRepository reviewRepository;
     private final MemberRepository memberRepository;
 
+    public Long getLike(Long reviewId, String memberId) {
+        Optional<Review> reviewOpt = reviewRepository.findById(reviewId);
+        Review review = reviewOpt.orElseThrow(() -> new ReviewNotFoundException("리뷰가 존재 하지 않습니다."));
+        Member member = memberRepository.findByMemberId(memberId);
+        Optional<Like> likeOpt = likeRepository.findByMemberAndReview(member,review);
+        return likeOpt.map(Like::getNo).orElse(0L);
+    }
+
+    @Transactional
     public void addLike(Long reviewId, String memberId) {
         Optional<Review> reviewOpt = reviewRepository.findById(reviewId);
         Review review = reviewOpt.orElseThrow(() -> new ReviewNotFoundException("리뷰가 존재 하지 않습니다."));
         Member member = memberRepository.findByMemberId(memberId);
         if(likeRepository.findByMemberAndReview(member,review).isEmpty()){
             likeRepository.save(Like.builder()
-                    .review(review)
-                    .member(member).build());
+                            .review(review)
+                            .member(member).build());
         }
+    }
+
+    @Transactional
+    public void deleteLike(Long reviewId, String memberId,Long likeId) {
+        reviewRepository.findById(reviewId).orElseThrow(() -> new ReviewNotFoundException("리뷰가 존재 하지 않습니다."));
+        Like like = likeRepository.findById(likeId).orElseThrow(() -> new LikeNotFoundException("좋아요가 없습니다."));
+        if (!memberId.equals(like.getMember().getMemberId())) {
+            throw new MemberIdMismatchException("멤버가 불일치 합니다.");
+        }
+        likeRepository.deleteById(likeId);
     }
 }
