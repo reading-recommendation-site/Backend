29d09f1332247cbd8417290f58e872c3728bf3fc
scarlet0o0 <ghd7673@naver.com>
2023-03-24 20:05:32 +0900

    [#34] Feat: 상세도서정보 컨트롤러단 구현

--- src/main/java/com/suggestion/book/domain/book/controller/BookSearchController.java
+++ src/main/java/com/suggestion/book/domain/book/controller/BookSearchController.java
@@ -2,8 +2,10 @@ package com.suggestion.book.domain.book.controller;
 
 import com.suggestion.book.domain.book.dto.BookListResponseDto;
 import com.suggestion.book.domain.book.service.BookSearchService;
+import com.suggestion.book.domain.recommendation.dto.DetailBookResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -16,5 +18,10 @@ public class BookSearchController {
     @GetMapping(path = "/search/book")
     public Mono<BookListResponseDto> getBookList(@RequestParam String title, @RequestParam int start){
         return bookSearchService.searchByBookTitle(title,start);
+    }
+
+    @GetMapping(path = "/search/book/{isbn}")
+    public Mono<DetailBookResponseDto> getBook(@PathVariable() String isbn) {
+        return bookSearchService.searchByBookISBN(isbn);
     }
 }
