b6ec94e7370ff04f4c153b653bbb511688d0c725
scarlet0o0 <ghd7673@naver.com>
2023-03-27 16:45:21 +0900

    [#36] Feat: member 정보 조회 api 구현

--- src/main/java/com/suggestion/book/domain/member/controller/MemberController.java
+++ src/main/java/com/suggestion/book/domain/member/controller/MemberController.java
@@ -1,10 +1,9 @@
 package com.suggestion.book.domain.member.controller;
 
-import com.suggestion.book.domain.member.entity.Member;
+import com.suggestion.book.domain.member.dto.MemberResponseDto;
 import com.suggestion.book.domain.member.service.MemberService;
-import com.suggestion.book.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +14,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping(path = "/member")
-    public ApiResponse<?> getUser() {
-        User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Member member = memberService.getMember(principal.getUsername());
-
-        return ApiResponse.success("member", member);
+    public MemberResponseDto getUser(@AuthenticationPrincipal User principal) {
+        return memberService.getMember(principal.getUsername());
     }
 }
