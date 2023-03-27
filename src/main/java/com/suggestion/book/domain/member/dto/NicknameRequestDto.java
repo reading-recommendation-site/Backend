14ecf5abeee60e41883ed69c03cffda794919f07
scarlet0o0 <ghd7673@naver.com>
2023-03-27 22:57:32 +0900

    [#40] Feat: NicknameRequestDto 생성

---
+++ src/main/java/com/suggestion/book/domain/member/dto/NicknameRequestDto.java
@@ -0,0 +1,10 @@
+package com.suggestion.book.domain.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NicknameRequestDto {
+    public String nickname;
+}
