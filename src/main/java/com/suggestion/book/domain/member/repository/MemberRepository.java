9dbbf843c9d017b92ae9142690fae192fb028e12
scarlet0o0 <ghd7673@naver.com>
2023-04-07 20:49:41 +0900

    [#72] Feat: 멤버 찾는 jpa 반환값 Optional 로 감싸기

--- src/main/java/com/suggestion/book/domain/member/repository/MemberRepository.java
+++ src/main/java/com/suggestion/book/domain/member/repository/MemberRepository.java
@@ -3,8 +3,9 @@ package com.suggestion.book.domain.member.repository;
 import com.suggestion.book.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByMemberId(String memberId);
+    Optional<Member> findByMemberId(String memberId);
 }
