b24bbd4a0bbb2208ece3979b77e624aa7c8bebe3
scarlet0o0 <ghd7673@naver.com>
2023-04-11 21:57:45 +0900

    [#76] Feat: EnableScheduling 어노테이션 추가

--- src/main/java/com/suggestion/book/BookApplication.java
+++ src/main/java/com/suggestion/book/BookApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ConfigurationPropertiesScan("com.suggestion.book.global.config.properties")
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class BookApplication {
