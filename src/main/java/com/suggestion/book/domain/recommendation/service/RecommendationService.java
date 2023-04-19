6482c96e8bf566134412b7ab7e00cf71903696e2
scarlet0o0 <ghd7673@naver.com>
2023-04-19 22:30:31 +0900

    [#76] Feat: 비교용 getPopularBook 함수 추가

--- src/main/java/com/suggestion/book/domain/recommendation/service/RecommendationService.java
+++ src/main/java/com/suggestion/book/domain/recommendation/service/RecommendationService.java
@@ -1,23 +1,28 @@
 package com.suggestion.book.domain.recommendation.service;
 
 import com.suggestion.book.domain.recommendation.dto.BestSellerListResponseDto;
+import com.suggestion.book.domain.recommendation.dto.PopularBookConditionsRequestDto;
 import com.suggestion.book.domain.recommendation.dto.PopularBookListResponseDto;
 import com.suggestion.book.domain.recommendation.exception.KeyNotFoundException;
 import com.suggestion.book.domain.recommendation.repository.PopularBookRedisRepository;
 import com.suggestion.book.global.config.properties.ApiProperties;
+import com.suggestion.book.global.utils.MultiValueMapConverterUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
 public class RecommendationService {
-
+    private final WebClient data4libraryWebClientApi;
     private final WebClient aladinWebClientApi;
     private final ApiProperties apiProperties;
     private final PopularBookRedisRepository popularBookRedisRepository;
+
     private static final String ALADIN_URI = "/ItemList.aspx";
+    private static final String POPULAR_BOOK_URI = "/loanItemSrch";
 
     public Mono<BestSellerListResponseDto> getBestSeller() {
         return aladinWebClientApi
@@ -50,6 +55,21 @@ public class RecommendationService {
                         .build())
                 .retrieve()
                 .bodyToMono(BestSellerListResponseDto.class);
+    }
+
+    public PopularBookListResponseDto getPopularBook(PopularBookConditionsRequestDto conditionsDto) {
+        MultiValueMap<String, String> params = MultiValueMapConverterUtil.convert(conditionsDto);
+        Mono<PopularBookListResponseDto> popularBookListResponseDtoMono = data4libraryWebClientApi
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(POPULAR_BOOK_URI)
+                        .queryParam("authKey", apiProperties.getNaru().getAuthKey())
+                        .queryParams(params)
+                        .queryParam("format","json")
+                        .build())
+                .retrieve()
+                .bodyToMono(PopularBookListResponseDto.class);
+        return popularBookListResponseDtoMono.block();
     }
 
     public PopularBookListResponseDto getPopularBook(String division) {
