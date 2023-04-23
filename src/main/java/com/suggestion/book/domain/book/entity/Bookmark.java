0bc211f23ace5c377369de7c291406c859d141c6
scarlet0o0 <ghd7673@naver.com>
2023-04-23 22:19:10 +0900

    [#78] Feat: Bookmark 에 Builder / time 추가

--- src/main/java/com/suggestion/book/domain/book/entity/Bookmark.java
+++ src/main/java/com/suggestion/book/domain/book/entity/Bookmark.java
@@ -1,12 +1,20 @@
 package com.suggestion.book.domain.book.entity;
 
+import com.suggestion.book.domain.community.dto.BookISBNResponseDto;
 import com.suggestion.book.domain.member.entity.Member;
+import com.suggestion.book.domain.model.TimestampEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "bookmark")
-public class Bookmark {
+public class Bookmark extends TimestampEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "bookmark_no")
@@ -27,4 +35,13 @@ public class Bookmark {
 
     @Column(name = "isbn")
     private String isbn;
+
+    @Builder
+    public Bookmark(Member member, String isbn, BookISBNResponseDto bookISBNResponseDto) {
+        this.member = member;
+        this.isbn = isbn;
+        this.bookTitle = bookISBNResponseDto.getItems().get(0).title;
+        this.bookAuthor = bookISBNResponseDto.getItems().get(0).author;;
+        this.bookImgUrl = bookISBNResponseDto.getItems().get(0).image;
+    }
 }
