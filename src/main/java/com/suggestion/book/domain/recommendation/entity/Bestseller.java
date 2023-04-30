cfb33ead0ba10391c145c42ba785e0d82a39afcf
scarlet0o0 <ghd7673@naver.com>
2023-04-30 17:43:53 +0900

    [#84] Feat: Redis Bestseller 엔티티 생성

---
+++ src/main/java/com/suggestion/book/domain/recommendation/entity/Bestseller.java
@@ -0,0 +1,25 @@
+package com.suggestion.book.domain.recommendation.entity;
+
+import com.suggestion.book.domain.recommendation.dto.BestSellerListResponseDto;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+import javax.persistence.Id;
+
+@RedisHash("Bestseller")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Bestseller {
+    @Id
+    private String id;
+    private BestSellerListResponseDto bestSellerListResponseDto;
+
+    @Builder
+    public Bestseller(String id, BestSellerListResponseDto bestSellerListResponseDto) {
+        this.id = id;
+        this.bestSellerListResponseDto = bestSellerListResponseDto;
+    }
+}
