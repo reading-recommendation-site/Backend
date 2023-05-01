ad4ece7e9cdf20096cf0dfaf27f2d0bd1f6ff251
scarlet0o0 <ghd7673@naver.com>
2023-05-01 20:51:29 +0900

    [#86] Feat: bestsellerSave 메소드 구현

--- src/main/java/com/suggestion/book/domain/recommendation/service/PopularBookSaveService.java
+++ src/main/java/com/suggestion/book/domain/recommendation/service/PopularBookSaveService.java
@@ -1,7 +1,10 @@
 package com.suggestion.book.domain.recommendation.service;
 
+import com.suggestion.book.domain.recommendation.dto.BestSellerListResponseDto;
 import com.suggestion.book.domain.recommendation.dto.PopularBookListResponseDto;
+import com.suggestion.book.domain.recommendation.entity.Bestseller;
 import com.suggestion.book.domain.recommendation.entity.PopularBook;
+import com.suggestion.book.domain.recommendation.repository.BestsellerRedisRepository;
 import com.suggestion.book.domain.recommendation.repository.PopularBookRedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,11 +15,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class PopularBookSaveService {
     private final PopularBookRedisRepository popularBookRedisRepository;
+    private final BestsellerRedisRepository bestsellerRedisRepository;
 
     public void popularBookSave(String key, PopularBookListResponseDto popularBookListResponseDto) {
         popularBookRedisRepository.save(PopularBook.builder()
                 .id(key)
                 .popularBookListResponseDto(popularBookListResponseDto)
+                .build()
+        );
+    }
+
+    public void bestsellerSave(String key, BestSellerListResponseDto bestSellerListResponseDto) {
+        bestsellerRedisRepository.save(Bestseller.builder()
+                .id(key)
+                .bestSellerListResponseDto(bestSellerListResponseDto)
                 .build()
         );
     }
